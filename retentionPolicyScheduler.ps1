1bec364fbf4c422e717780b2d4098aa4d69f1246
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-08-08 09:57:43 -0500

    Add files via upload

---
+++ retentionPolicyScheduler.ps1
@@ -0,0 +1,8 @@
+﻿# Delete all Files in C:\temp older than 30 day(s)
+$Path = "C:\file\path\to\server" #replace the path
+$Daysback = "-5" #Change to any amount of days to keep retention policy
+$string = "*log*" #Replace 'log' with the word you're looking for.
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path -Recurse -Include $string | Where{ $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
