bbe39ba982879b5f38b8817161fad61c3e19c948
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2023-07-19 12:48:50 -0500

    Update retentionPolicyScheduler.ps1

--- retentionPolicyScheduler.ps1
+++ retentionPolicyScheduler.ps1
@@ -1,8 +1,9 @@
-﻿# Delete all Files in C:\temp older than 30 day(s)
-$Path = "C:\file\path\to\server" #replace the path
-$Daysback = "-5" #Change to any amount of days to keep retention policy
-$string = "*log*" #Replace 'log' with the word you're looking for.
+
+$Path = "C:/dir/to/Log" #replace the path
+$num = Read-Host("Enter number here: ")
+$Daysback = $num #Change to any amount of days to keep retention policy
+$string = "*.log" #Log file extension
  
 $CurrentDate = Get-Date
-$DatetoDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $Path -Recurse -Include $string | Where{ $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+$DatetoDelete = $CurrentDate.AddDays("-"+$Daysback)
+Get-ChildItem $Path -Recurse -Filter $string | Where-Object{ $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
